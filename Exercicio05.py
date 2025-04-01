b67a2d38c7b83e34a3a73e5b6825e593ed0d3311
Adyla Souza <adyla.projetos@gmail.com>
2025-04-01 15:57:53 -0300

    Média de três notas

---
+++ Exercicio05.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Digita a 1 nota"))
+nota2 = float(input("Digita a 2 nota"))
+nota3 = float(input("Digita a 3 nota"))
+
+media = (nota1+nota2+nota3)/3
+
+if media >= 7:
+    print(f"aprovado {media}")
+else:
+    print(f"reprovado{media}")
