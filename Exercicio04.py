fb7de1767a83f2dd44941e725b75af0e70b89f37
Adyla Souza <adyla.projetos@gmail.com>
2025-04-01 15:47:46 -0300

    Ordem decrescente

---
+++ Exercicio04.py
@@ -0,0 +1,13 @@
+"""a = 8
+b = 7
+if a>b:
+    print("A é maior")
+else:
+    print("B é o maior")"""
+
+n1 = int(input("Informe o primeiro número: "))
+n2 = int(input("Informe o segundo número: "))
+if n1 > n2:
+    print(n2,n1)
+else:
+    print(n1,n2)
