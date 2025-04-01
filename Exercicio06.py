9c47e381d245b4ef20e2c53fb0433875dbc136af
Adyla Souza <adyla.projetos@gmail.com>
2025-04-01 16:29:47 -0300

    Time vencedor

---
+++ Exercicio06.py
@@ -0,0 +1,10 @@
+time1 = input("Informe o nome do  primeiro time: ")
+gols1 =  int(input("informe o número de gols: "))
+time2 = input("Informe o nome do  primeiro time: ")
+gols2 =  int(input("informe o número de gols: "))
+
+if gols2>gols1:
+    print(f"Time do {time2} é o vencedor")
+else:
+    print(f"Time do {time1} é o vencedor")
+
