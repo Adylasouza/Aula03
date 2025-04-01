b09a3e357205597a62563b80df34612cb31fe8d4
Adyla Souza <adyla.projetos@gmail.com>
2025-04-01 14:38:26 -0300

    Calculo de valores

---
+++ Exercicio02.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Informe o primeiro número: "))
+numero2 = int(input("Informe o segundo número: "))
+soma = numero1 + numero2
+print(f"O valor da soma é : {soma}")
+subtracao = numero1 - numero2
+print(f"O valor da subtração é : {subtracao}")
+divisao = numero1 / numero2
+print(f"O valor da divisão é : {divisao}")
+multiplicacao = numero1 * numero2
+print(f"O valor da multiplicação é : {multiplicacao}")
