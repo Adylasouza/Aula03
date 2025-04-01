f3da480bcd6a40905ef1e3ed0fee6239249e46ef
Adyla Souza <adyla.projetos@gmail.com>
2025-04-01 16:08:18 -0300

    Média de três notas, mostrando recuperação

--- Exercicio05.py
+++ Exercicio05.py
@@ -1,10 +1,13 @@
-nota1 = float(input("Digita a 1 nota"))
-nota2 = float(input("Digita a 2 nota"))
-nota3 = float(input("Digita a 3 nota"))
+nota1 = float(input("Digita a 1 nota: "))
+nota2 = float(input("Digita a 2 nota: "))
+nota3 = float(input("Digita a 3 nota: "))
 
 media = (nota1+nota2+nota3)/3
 
 if media >= 7:
     print(f"aprovado {media}")
 else:
-    print(f"reprovado{media}")
+    if media <=4:
+        print(f"reprovado {media}")
+    else:
+        print(f"recuperação {media}")
