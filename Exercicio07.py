bce4b80f359784314c71a9b9ca91f612a0ef4531
Adyla Souza <adyla.projetos@gmail.com>
2025-04-03 14:25:37 -0300

    Valor gasto combustivel corrigido

--- Exercicio07.py
+++ Exercicio07.py
@@ -2,9 +2,15 @@ tipoCombustivel= input("Informe qual tipo de combustível foi abastecido (G para
 litros = float(input("Informe o tipo de combustível abastecido: " ))
 vGas = 5.8
 vEta = 4.9
-if tipoCombustivel == "G":
+if tipoCombustivel == "G" or tipoCombustivel == "g":
     valor = vGas * litros
-else:
-    valor =  vEta * litros
+    print(f"Você pagará: {valor:.2f} ")
 
-print(f"Você pagará: {valor:.2f} ")
+#elif tipoCombustivel == "e" or tipoCombustivel == "E"
+else:
+    if tipoCombustivel == "E" or tipoCombustivel == "e":
+        valor =  vEta * litros
+        print(f"Você pagará: {valor:.2f} ")
+    else:
+        print("Tipo de combustível inválido")
+
