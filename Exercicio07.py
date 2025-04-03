b89ba5d4f7e0b531367e0c71a980c85f61dab3b2
Adyla Souza <adyla.projetos@gmail.com>
2025-04-03 14:06:51 -0300

    Valor gasto combustivel

---
+++ Exercicio07.py
@@ -0,0 +1,10 @@
+tipoCombustivel= input("Informe qual tipo de combustível foi abastecido (G para Gasolina e E para Etanol):  ")
+litros = float(input("Informe o tipo de combustível abastecido: " ))
+vGas = 5.8
+vEta = 4.9
+if tipoCombustivel == "G":
+    valor = vGas * litros
+else:
+    valor =  vEta * litros
+
+print(f"Você pagará: {valor:.2f} ")
